abb8b654bb43050e1af2aef6403d305e60457d8c
mxm381 <78032612+mxm381@users.noreply.github.com>
2023-04-03 20:51:21 +0200

    ojnoi

--- webapp/app.py
+++ webapp/app.py
@@ -1,36 +1,42 @@
-import os
 import requests
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    #return render_template('index.html')
+    return send_file("index.html")
 
 @app.route('/detect', methods=['POST'])
 def detect():
-    auth_key = os.environ.get('DEEPL_KEY')
+    #auth_key = os.environ.get('AZURE_KEY')
+    auth_key = "c3f3596e-e15d-45a3-b995-af4eb8a8933d"
 
     text = request.form['text-input']
-    target_language = request.form['language-select']
     
-    url = "https://api-free.deepl.com/v2/translate"
+    url = "https://cloud-languagedetection.cognitiveservices.azure.com/"
 
     headers = {
-        "Authorization": f"DeepL-Auth-Key {auth_key}"
+        "Authorization": f"Azure-Auth-Key {auth_key}"
     }
 
     data = {
-        "text": text,
-        "target_lang": target_language
+        "query": text
     }
 
     response = requests.post(url, headers=headers, data=data)
-    
-    translated_text = response.json()['translations'][0]['text']
+    response_dict = response.json()
+    print(response_dict)
 
-    return render_template('index.html', output=translated_text)
+    
+    #translated_text = response.json()['translations'][0]['text']
+    detected_language = response_dict['prediction']['intents']['Delivery']['childApp']['topIntent']
+    #detected_language = response.json()['prediction'[0]]['topIntent']
+    
+
+    #return render_template('index.html', output=detected_language)
+    return send_file("index.html", output = detected_language)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
